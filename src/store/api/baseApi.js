82a1c4955feda658ff9f692e4b82d525fe24083c
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2024-11-24 18:45:41 +0600

    deployment

--- src/store/api/baseApi.js
+++ src/store/api/baseApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/",
+  baseUrl: "https://myself-captake-server.vercel.app/",
   // credentials: "include",
 });
 
